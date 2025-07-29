9567295a6b5633771c2cb39d4968713a37a33239
theoneandnoely <nmarkham96@gmail.com>
2025-07-29 11:14:51 +0100

    Updated animation to be a cirle

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // TODO(Noel): Look into best method for importing packages/modules
-import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
+// import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
+
+// import { select, range, line } from 'd3';
 
 let width = window.innerWidth;
 let height = window.innerHeight;
 
-let svg = d3.select('body').append('svg');
+const svg = d3.select('body').append('svg');
 svg
     .attr('width',width)
     .attr('height',height)
@@ -20,31 +22,15 @@ setInterval(() => {
     height = window.innerHeight;
     svg.attr('width',width).attr('height',height);
 
-    const n = 10 + (Math.sin(t/20) * 10); // variability to range size
+    const n = 6 + (Math.sin(t/40) * 6); // variability to range size
     const data = d3.range(n).map((d) => ({
-        x: d * (width/22) + 50,
+        x: width/2 + (Math.cos(d * 0.5 + (t/20)) * (width-100)/2),
         y: height/2 + Math.sin(d * 0.5 + (t/20)) * ((height-100)/2),
         r: 20 + (Math.sin(d * 0.5 + t/20)*5),
-        fill: `rgba(${(Math.cos(d*0.5)*50) + (Math.sin(t/20)*120)},${(Math.sin(d*0.5)*50) + (Math.cos(t/20)*120)}, ${(Math.cos(d*0.5)*50) + (Math.sin(t/20)*120)}, 1)`
+        fill: `rgba(${(Math.sin(d/20)*50) + (Math.sin(t/50)*100)},${(Math.sin(d/20)*50) + (Math.cos(t/50)*100)}, ${(Math.sin(d/20)*50) + (Math.sin(t/20)*100)}, 1)`
     }));
 
-    // const circles = svg
-    //     .selectAll('circle')
-    //     .data(data)
-    // ;
-    // const circlesEnter = circles
-    //     .enter()
-    //     .append('circle')
-    // ;
-    // circles.merge(circlesEnter)
-    //     .attr('r',20)
-    //     .attr('cx',(d) => d.x)
-    //     .attr('cy',(d) => d.y)
-    // ;
-    // circles.exit().remove();
-
-    // Equivalent to the above using only join
-    const circles = svg
+    svg
         .selectAll('circle')
         .data(data)
         .join('circle') // enters the selection on first pass through, merges with the update on subsequent pass throughs, exits and removes any elements when they are no longer in the data
@@ -52,6 +38,20 @@ setInterval(() => {
         .attr('cx', (d) => d.x)
         .attr('cy', (d) => d.y)
         .attr('fill', (d) => d.fill)
+    ;
+
+    const lineGenerator = d3.line()
+        .x((d) => d.x)
+        .y((d) => d.y)
+    ;
+
+    svg.selectAll('path')
+        .data([null])
+        .join('path')
+        .attr('d', lineGenerator(data))
+        .attr('fill', 'none')
+        .attr('stroke','black')
+        .attr('stroke-width',3)
     ;
     t++;
 }, 1000 / fps);
