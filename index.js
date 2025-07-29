5fc523dd796494911b2f7627bc88da686a769ded
theoneandnoely <nmarkham96@gmail.com>
2025-07-29 09:57:13 +0100

    Exercise 5: Exploring select.exit

--- index.js
+++ index.js
@@ -20,19 +20,38 @@ setInterval(() => {
     height = window.innerHeight;
     svg.attr('width',width).attr('height',height);
 
-    const n = 20;
-    const data = d3.range(20).map((d) => ({
-        x: (d * ((width - (width/(n+2))) / n)) + (width/(n+2)),
-        y: (height/2) + Math.sin(d * 0.5 + (t/20)) * ((height -100) / 2)
+    const n = 10 + (Math.sin(t/20) * 10); // variability to range size
+    const data = d3.range(n).map((d) => ({
+        x: d * (width/22) + 50,
+        y: height/2 + Math.sin(d * 0.5 + (t/20)) * ((height-100)/2),
+        r: 20 + (Math.sin(d * 0.5 + t/20)*5),
+        fill: `rgba(${(Math.cos(d*0.5)*50) + (Math.sin(t/20)*120)},${(Math.sin(d*0.5)*50) + (Math.cos(t/20)*120)}, ${(Math.cos(d*0.5)*50) + (Math.sin(t/20)*120)}, 1)`
     }));
 
+    // const circles = svg
+    //     .selectAll('circle')
+    //     .data(data)
+    // ;
+    // const circlesEnter = circles
+    //     .enter()
+    //     .append('circle')
+    // ;
+    // circles.merge(circlesEnter)
+    //     .attr('r',20)
+    //     .attr('cx',(d) => d.x)
+    //     .attr('cy',(d) => d.y)
+    // ;
+    // circles.exit().remove();
+
+    // Equivalent to the above using only join
     const circles = svg
         .selectAll('circle')
         .data(data)
-        .join('circle') // enters the selection on first pass through and merges with the update on subsequent pass throughs
-        .attr('r',20)
+        .join('circle') // enters the selection on first pass through, merges with the update on subsequent pass throughs, exits and removes any elements when they are no longer in the data
+        .attr('r',(d) => d.r)
         .attr('cx', (d) => d.x)
         .attr('cy', (d) => d.y)
+        .attr('fill', (d) => d.fill)
     ;
     t++;
 }, 1000 / fps);
